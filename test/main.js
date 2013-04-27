5f90c9087a5ede4394b3e85ce603dd076c0d3771
Markus Pielmeier <markus.pielmeier@googlemail.com>
2013-04-27 14:12:30 +0200

    implemented bounds parsing

--- test/main.js
+++ test/main.js
@@ -1,1 +1,30 @@
+var should = require('should');
+var osmread = require('../lib/main');
 
+describe('osmread', function(){
+    describe('when xml with invalid syntax is parsed', function(){
+        it('then an error should be raised', function(done){
+            osmread.parse({
+                filePath: 'test/main.js',
+                error: function(msg){
+                    done();
+                }
+            });
+        });
+    });
+
+    describe('when test.xml is parsed', function(){
+        it('then bounds callback should min and max lat and long', function(done){
+            osmread.parse({
+                filePath: 'test/test.xml',
+                bounds: function(bounds){
+                    bounds.minlat.should.be.within(51.507360179555, 51.507360179556);
+
+                    done();
+                }
+            });
+
+            
+        });
+    });
+});
