e1f91676c8888202c51148b816837e654091bbcc
Markus Pielmeier <markus.pielmeier@googlemail.com>
2013-04-27 14:34:40 +0200

    now node ids are parsed

--- test/test.js
+++ test/test.js
@@ -48,5 +48,9 @@ describe('osmread', function(){
         it('then node callback should deliver 6 nodes', function(){
             parsedNodes.length.should.eql(6);
         });
+
+        it('then first parsed node has id 319408586', function(){
+            parsedNodes[0].id.should.eql('319408586');
+        });
     });
 });
