203d655184b39b8f71aa0a40794b306e3ab5e135
Markus Pielmeier <markus.pielmeier@googlemail.com>
2013-05-15 21:22:15 +0200

    added inflation support for zlib data

--- test/pbfTest.js
+++ test/pbfTest.js
@@ -6,7 +6,15 @@ describe('pbf read', function(){
     it('just run the stuff', function(done){
         // this.timeout(60 * 1000);
 
-        osmread.parsePbf(done);
+        osmread.parsePbf(function(err){
+            if(err){
+                console.log(err);
+
+                should.fail();
+            }
+
+            return done();
+        });
     });
 
 });
