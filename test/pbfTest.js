ade0b49dcdf534ac0384d7204f2d262be06616df
Markus Pielmeier <markus.pielmeier@googlemail.com>
2013-05-20 13:59:37 +0200

    adjusted pbf parser's API to xml parser's api + added some tests

--- test/pbfTest.js
+++ test/pbfTest.js
@@ -3,18 +3,65 @@ var osmread = require('../lib/main');
 
 describe('pbf read', function(){
     
-    it('just run the stuff', function(done){
-        // this.timeout(60 * 1000);
+    describe('when parser for test.pbf exists', function(){
+        var parser;
+        
+        before(function(done){
+            osmread.parsePbf({
+                filePath: 'test/test.pbf',
+                callback: function(err, p){
+                    if(err){
+                        should.fail();
+                        return done();
+                    }
 
-        osmread.parsePbf(function(err){
-            if(err){
-                console.log(err);
+                    parser = p;
 
-                should.fail();
-            }
-
-            return done();
+                    return done();
+                }
+            });
         });
+
+        after(function(done){
+            parser.close(function(err){
+                if(err){
+                    should.fail();
+                    return done();
+                }
+
+                return done();
+            });
+        });
+
+        it('has one OSMHeader', function(){
+            var blocks = parser.findFileBlocksByBlobType('OSMHeader');
+
+            blocks.length.should.be.equal(1);
+        });
+
+        describe('and first OSMHeader exists', function(){
+            var osmHeaderBlock, osmHeader;
+
+            before(function(done){
+                osmHeaderBlock = parser.findFileBlocksByBlobType('OSMHeader')[0];
+                parser.readBlock(osmHeaderBlock, function(err, block){
+                    if(err){
+                        should.fail();
+
+                        return done();
+                    }
+
+                    osmHeader = block;
+
+                    return done();
+                });
+            });
+
+            it('then writingprogram is "0.40.1"', function(){
+                osmHeader.writingprogram.should.be.equal('0.40.1');
+            });
+        });
+
     });
 
 });
