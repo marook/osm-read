363529fc3a79252239b22f3632da88b978dac489
Markus Peröbner <markus.peroebner@gmail.com>
2014-05-21 19:57:40 +0200

    added test for parsing relation member ways in XML

--- test/xmlPbfTest.js
+++ test/xmlPbfTest.js
@@ -193,6 +193,21 @@ describe('osmread', function(){
             nodeMember.ref.should.be.equal('319408586');
             nodeMember.role.should.be.equal('');
         });
+
+        it('then first relation\'s member ways are parsed', function(){
+            /*
+             * Currently relations are only parsed in XML files. That's
+             * why this test is not part of the format independent test
+             * suite.
+             */
+
+            var nodeMember;
+
+            nodeMember = params.parsedRelations[0].members.ways[0];
+
+            nodeMember.ref.should.be.equal('27776903');
+            nodeMember.role.should.be.equal('');
+        });
     });
 
     describeTest('test/test.pbf', function(parsedBounds, parsedNodes, parsedWays){
